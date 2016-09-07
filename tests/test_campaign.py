641c68273bd754b80dc20a4dc0953f3d1fc262bc
Cerilla <weiss.cerilla@gmail.com>
2016-09-07 21:05:50 +0700

    Add comment to test functions

--- tests/test_campaign.py
+++ tests/test_campaign.py
@@ -7,6 +7,8 @@ import youseidm
 sample_dir = os.path.join(os.path.dirname(__file__), 'sampledata')
 
 def test_campaign_init():
+    """Initialize a new campaign with sample game json file.
+    """
     sample = os.path.join(sample_dir, 'samplegame.json')
     game = youseidm.GameCampaign(sample)
     assert game.currentgame.gamename == 'Sample Homebrew Game'
--- tests/test_gamedata.py
+++ tests/test_gamedata.py
@@ -9,13 +9,17 @@ sample_dir = os.path.join(os.path.dirname(__file__), 'sampledata')
 
 
 def test_gamedata_init():
-    # Read the gamefile.json and populate it in
+    """Initialize GameData object with a sample json information
+    """
     sample = os.path.join(sample_dir, 'samplegame.json')
     game = yg.GameData(sample)
     assert game.gamename == 'Sample Homebrew Game'
 
 
 def test_gamedata_init_without_name():
+    """Intialize GameData object with a sample without gamedata field.
+    When it is missing, the initializer should raise ValueError.
+    """
     sample = os.path.join(sample_dir, 'sample_noname.json')
     with pytest.raises(ValueError):
         game = yg.GameData(sample)
