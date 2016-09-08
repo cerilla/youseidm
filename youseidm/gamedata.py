8d3186a206ab92bd49538735496096205fedf04a
Cerilla <weiss.cerilla@gmail.com>
2016-09-08 08:04:27 +0700

    Do small housekeeping

--- youseidm/gamedata.py
+++ youseidm/gamedata.py
@@ -19,7 +19,6 @@ class GameData(object):
 
         with open(gamefile) as f:
             info = json.loads(f.read())
-            print(info)
             for k, v in info.items():
                 setattr(self, k, v)
 
